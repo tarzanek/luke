bd8a4a8e8b1c0147cb8c03ff20b11db5d4eb3314
Lubos Kosco <tarzanek@gmail.com>
2016-02-22 13:47:53 +0100

    lucene 5.5.0, elasticssearch 2.2.0

--- src/org/getopt/luke/NoScoringScorer.java
+++ src/org/getopt/luke/NoScoringScorer.java
@@ -1,45 +1,58 @@
 package org.getopt.luke;
 
 import java.io.IOException;
+import org.apache.lucene.search.DocIdSetIterator;
 
 import org.apache.lucene.search.Scorer;
 
 public class NoScoringScorer extends Scorer {
-  public static final NoScoringScorer INSTANCE = new NoScoringScorer();
 
-  protected NoScoringScorer() {
-    super(null);
-  }
+    public static final NoScoringScorer INSTANCE = new NoScoringScorer();
 
-  @Override
-  public float score() throws IOException {
-    return 1.0f;
-  }
-
-  @Override
-  public int advance(int doc) throws IOException {
-    return 0;
-  }
-
-  @Override
-  public int docID() {
-    return 0;
-  }
-
-  @Override
-  public int nextDoc() throws IOException {
-    return 0;
-  }
-
-  @Override
-  public int freq() throws IOException {
-    // TODO Auto-generated method stub
-    return 1;
-  }
+    protected NoScoringScorer() {
+        super(null);
+    }
 
     @Override
-    public long cost() {
+    public float score() throws IOException {
+        return 1.0f;
+    }
+
+    @Override
+    public int docID() {
         return 0;
+    }
+
+    @Override
+    public int freq() throws IOException {
+        // TODO Auto-generated method stub
+        return 1;
+    }
+
+    @Override
+    public DocIdSetIterator iterator() {
+        return new DocIdSetIterator() {
+            @Override
+            public int docID() {
+                return 0;
+            }
+
+            @Override
+            public int nextDoc() throws IOException {
+                return 0;
+            }
+
+            @Override
+            public int advance(int i) throws IOException {
+                return 0;
+            }
+
+            @Override
+            public long cost() {
+                return 0;
+            }
+
+        };
     }
 
 }
